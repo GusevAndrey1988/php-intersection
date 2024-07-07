3723f704cefb5cb81e9c30a576100167e46d2ccf
Andrey <GusevAndrey1988@gmail.com>
2024-07-07 21:41:17 +0300

    refactor Math2D

--- src/Math2D.php
+++ src/Math2D.php
@@ -6,7 +6,7 @@ namespace Lightsaber\PhpIntersection;
 
 class Math2D
 {
-    public const DEFAULT_EPSILON = 0.0000000001;
+    public const DEFAULT_EPSILON = 0.000000000001;
 
     public static function closestPointOnLine(
         Vector2D $point,
@@ -21,10 +21,14 @@ class Math2D
         return $start->sum($a->mul($c));
     }
 
-    public static function edgeNormal(Vector2D $a, Vector2D $b): Vector2D
-    {
+    public static function edgeNormal(
+        Vector2D $a,
+        Vector2D $b,
+        Epsilon $epsilon = new Epsilon(self::DEFAULT_EPSILON)
+    ): Vector2D {
         $edge = $b->sub($a);
-        return (new Vector2D(-$edge->y(), $edge->x()))->normalize();
+        return (new Vector2D(-$edge->y(), $edge->x()))
+            ->normalize($epsilon);
     }
 
     public static function clamp(float $value, float $min, float $max): float
